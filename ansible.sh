90451173675ed107469393decd1e183700c48ce0
Saleh Borhani <95637102+SalehBorhani@users.noreply.github.com>
2023-10-06 16:20:13 +0330

    fix: changed the installation

--- ansible.sh
+++ ansible.sh
@@ -1,7 +1,1 @@
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py --user
-
-python3 -m pip install --user ansible
-
-echo "export PATH=/root/.local/bin:$PATH" >> ~/.bashrc
-source ~/.bashrc
+apt install ansible
